f44237eb61f86f31f9ca25754fe980cb8864f0bf
Le <liu00414@algonquinlive.com>
2017-12-22 18:38:50 -0500

    initial commit

---
+++ js/starter.js
@@ -0,0 +1,6 @@
+let contactStarter = [
+    {"fullname":"Le Liu","phone":"613-794-6508","email":"anbylele@gmail.com"},
+//    {"fullname":"Han Solo","phone":"613-727-4723","email":"han.solo@falcon.com"},
+//    {"fullname":"Luke Skywalker","phone":"613-747-4922","email":"luke.skywalker@jedi.org"},
+//    {"fullname":"Kylo Ren","phone":"613-997-6423","email":"kylo.ren@firstorder.io"}
+];
